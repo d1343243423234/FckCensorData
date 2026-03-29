ec0c95aa272a8791f2de63e1113d8488c954d474
Hazzz895 <kornilovily4@gmail.com>
2026-03-29 15:05:55 +0400

    refactor

--- append_track.py
+++ append_track.py
@@ -3,39 +3,40 @@ import sys
 import json
 from git import Repo
 
-id = input("Yandex Music track ID or URL: ").split('/')[-1].split('?')[0]
+while True:
+    id = input("Yandex Music track ID or URL: ").split('/')[-1].split('?')[0]
 
-print("Fetching track info...")
-track_name = id
-try:
-    from yandex_music import Client
-    client = Client().init()
-    track_info = client.tracks([id])[0]
-    track_name = f'{track_info.title} - {(", ".join(track_info.artistsName()))}'
-except Exception as e:
-    print(f"Could not fetch track info: {e}")
+    print("Fetching track info...")
+    track_name = id
+    try:
+        from yandex_music import Client
+        client = Client().init()
+        track_info = client.tracks([id])[0]
+        track_name = f'{track_info.title} - {(", ".join(track_info.artistsName()))}'
+    except Exception as e:
+        print(f"Could not fetch track info: {e}")
 
-url = input("Track URL: ")
+    url = input("Track URL: ")
 
-repo = Repo('.')
+    repo = Repo('.')
 
-def download_sound(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': f'tracks/{id}.%(ext)s',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    def download_sound(url):
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': f'tracks/{id}.%(ext)s',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-download_sound(url)
+    download_sound(url)
 
-with open('list.json', 'r') as f:
-    data = json.loads(f.read())
-    data['tracks'][id] = url
+    with open('list.json', 'r') as f:
+        data = json.loads(f.read())
+        data['tracks'][id] = f'https://raw.githubusercontent.com/Hazzz895/FckCensorData/refs/heads/main/tracks/{id}'
 
-with open('list.json', 'w') as f:
-    json.dump(data, f)
+    with open('list.json', 'w') as f:
+        json.dump(data, f)
 
-repo.index.add(['list.json', 'tracks/'])
-repo.index.commit(f"add track «{track_name}»")
-print(f'Successfully added track {track_name}')
+    repo.index.add(['list.json', 'tracks/'])
+    repo.index.commit(f"add track «{track_name}»")
+    print(f'Successfully added track {track_name}')
